3c463950b049a91e8212f6134a1dcdf87a061516
Diogo Alves <diogomoura2014@gmail.com>
2023-02-24 16:49:30 +0000

    created GameState class

---
+++ GameState.java
@@ -0,0 +1,13 @@
+class GameState {
+  // constructor
+  private int[] board;
+
+  public GameState(int n) {
+    // create a new game
+    board = new int[n * n];
+  }
+
+  void read_board() {
+
+  }
+}
