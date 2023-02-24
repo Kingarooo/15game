65b310640610d093c9cc969c5ff1496205601c09
Diogo Alves <diogomoura2014@gmail.com>
2023-02-24 17:35:15 +0000

    foda se esta merda

--- main.java
+++ main.java
@@ -4,9 +4,10 @@ public class main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        for (int k = 0; k < 5; k++) {
+        for (int k = 0; k < 1; k++) {
             GameState board = new GameState(4);
-            board.read_board();
+            board.read_board(in);
+            int zeroPosition = board.getZeroPosition();
         }
 
     }
