4b26a6848032f2902acc06ff2c52420b290bd1c4
Diogo Alves <diogomoura2014@gmail.com>
2023-02-25 16:36:27 +0000

    resolvi uma cena a ler board outra vez

--- GameState.java
+++ GameState.java
@@ -21,8 +21,7 @@ class GameState {
     return size;
   }
 
-  public void read_board() {
-    Scanner in = new Scanner(System.in);
+  public void read_board(Scanner in) {
     int index = 0;
     for (int i = 0; i < this.size; i++) {
       for (int j = 0; j < this.size; j++) {
@@ -64,15 +63,15 @@ class GameState {
     return inversions;
   }
 
-  public boolean HelperSolution(int inversions) {
+  public boolean Solvable(int inversions) {
+    for (int i = 0; i < this.getSize(); i++) {
+      for (int j = i + 1; j < this.getSize() - 1; j++) {
+        if (this.at(i) != 0 && this.at(j) != 0 && this.at(i) > this.at(j))
+          inversions++;
+      }
+    }
+    System.out.println(inversions);
     return inversions % 2 == 0;
   }
 
-  public void Solvable(){
-    if (this.HelperSolution(this.Inversions()) == ((zeroPosition/4 + 1) % 2 == 0))
-        System.out.println("É solvable");
-    else 
-        System.out.println("Não é solvable");
-    return;
-  }
 }
