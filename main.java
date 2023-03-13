faf88aec6b85e7041f132d69eb97f442da0b3c5f
Diogo Alves <diogomoura2014@gmail.com>
2023-03-13 03:50:41 +0000

    Tudo feito caralho

--- main.java
+++ main.java
@@ -14,7 +14,7 @@ public class main {
             case "DFS":
                 if (board.isSolvable() == goalboard.isSolvable()) {
                     DFS dfs = new DFS(board, goalboard);
-                    dfs.search(16);
+                    dfs.search(25);
                     return;
                 } else {
                     System.out.println("No solution");
